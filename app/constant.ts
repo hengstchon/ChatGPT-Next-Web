fb9bfb60e17e076981bf6da2f92cd3136d23587b
hengstchon <hengstchon@gmail.com>
2024-12-16 09:13:52 +0100

    feat: add anthropic model: claude-3-5-sonnet-latest

--- app/constant.ts
+++ app/constant.ts
@@ -337,7 +337,6 @@ const anthropicModels = [
   "claude-3-5-haiku-20241022",
   "claude-3-5-haiku-latest",
   "claude-3-5-sonnet-20240620",
-  "claude-3-5-sonnet-20241022",
   "claude-3-5-sonnet-latest",
 ];
 
